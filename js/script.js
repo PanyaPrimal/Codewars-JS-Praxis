88dcf36c7409d746bf26423331042b124df3fddc
Victor <vpanya97@gmail.com>
2022-02-11 19:39:16 +0200

    points counter for football champ

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
-let s = "aaaaaaaaaaaaaaaabbbbbxvciubbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+gamesArr = ["1:2","2:2","3:4","4:3","2:2","3:3","5:4","3:3","4:4","1:4"];
+
+function points(games) {
+    let points = 0;
+    for (let i = 0; i < games.length; i++) {
+        
+        let x = games[i].charAt(0);
+        let y = games[i].charAt(2);
+        
+        if (x > y) {
+            points = points + 3;
+        } else if (x == y ){
+            points = points + 1;
+        }
+
+    }
+return points;
+}
+
+  points(gamesArr);
  
-const printerError = s => `${s.replace(/[a-m]/gi, '').length}/${s.length}`;
-
-    console.log(printerError(s));
-
-
-
-
-
 
 /** 
+ * 
+__________________________________________________________
+Our football team finished the championship. The result of each
+match look like "x:y". Results of all matches are recorded in the 
+collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of
+our team in the championship. Rules for counting points for each
+match:
+
+if x>y - 3 points
+if x<y - 0 point
+if x=y - 1 point
+
+Sol: 
+
+
+
+__________________________________________________________
  * 
  * In a factory a printer prints labels for boxes. For one kind 
  * of boxes the printer has to use colors which, for the sake of
@@ -46,6 +78,13 @@ function printerError(s) {
 }
 
 printerError(a);
+_____
+const printerError = s => `${s.replace(/[a-m]/gi, '').length}/${s.length}`;
+
+    console.log(printerError(s));
+
+
+
  * __________________________________________________________
 
  *
