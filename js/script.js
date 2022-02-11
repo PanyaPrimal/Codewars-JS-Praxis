5009967c54766c2967af117e4fce66be5069d3d1
Victor <vpanya97@gmail.com>
2022-02-11 18:59:44 +0200

    Add replacer of useless symbols with regExp

--- js/script.js
+++ js/script.js
@@ -1,11 +1,53 @@
-let someDna = "TGACCGTCCGCC";
+let s = "aaaaaaaaaaaaaaaabbbbbxvciubbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+ 
+const printerError = s => `${s.replace(/[a-m]/gi, '').length}/${s.length}`;
 
-function DNAtoRNA(dna) {
-    return dna.replace(/T/g, "U");
-}
-  DNAtoRNA(someDna);
+    console.log(printerError(s));
+
+
+
+
+
+
 /** 
+ * 
+ * In a factory a printer prints labels for boxes. For one kind 
+ * of boxes the printer has to use colors which, for the sake of
+ *  simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. 
+For example a "good" control string would be aaabbbbhaijjjm meaning
+ that the printer used three times color a, four times color b, one
+  time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction
+ and a "bad" control string is produced e.g. 
+aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string
+ will return the error rate of the printer as a string representing
+  a rational whose numerator is the number of errors and the denominator the length
+   of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only
+ letters from ato z.
+
+ Sol:
+
+let a = "aaaaaaaaaaaaaaaabbbbbxvciubbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+function printerError(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] > "m") {
+            count++;
+        }
+    }
+    console.log(count+"/"+s.length);
+}
+
+printerError(a);
  * __________________________________________________________
+
  *
 
 Create a function which translates a given DNA string into RNA.
@@ -19,7 +61,12 @@ of 'G', 'C', 'A' and/or 'T'.
 
 Sol:
 
+let someDna = "TGACCGTCCGCC";
 
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, "U");
+}
+  DNAtoRNA(someDna);
 
 __________________________________________________________
 
