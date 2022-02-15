f94ca9a0555ffba0975a136e9c13d14d30b9c5c1
Victor <vpanya97@gmail.com>
2022-02-15 19:15:51 +0200

    arr[i] vs arr[i*i] comparor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-function boolToWord( bool ){
-    return bool ? "Yes" : "No"
-  }
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+function comp(array1, array2){
+    if (array1 !==null & array2 !== null && array1.length == array2.length) {
+        array1.sort((a, b) => a - b); array2.sort((a,b) => a - b);
+        return array1.map(v => v * v).every((v, i) => v == array2[i]);
+    } return false;
+}
+
+comp(a, b);
 /** 
- 
+_________________________________________________________
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) 
+that checks whether the two arrays have the "same" elements, with the same 
+multiplicities (the multiplicity of a member is the number of times it appears). 
+"Same" means, here, that the elements in b are the elements in a squared, 
+regardless of the order.
+
+Sol: 
+
+
 _________________________________________________________
 Complete the method that takes a boolean value and return a "Yes" 
 string for true, or a "No" string for false.
 
 Sol:
+function boolToWord( bool ){
+    return bool ? "Yes" : "No"
+  }
 __________________________________________________________
 Our football team finished the championship. The result of each
 match look like "x:y". Results of all matches are recorded in the 
