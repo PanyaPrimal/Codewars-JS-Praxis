4b494d7c10c0ef1a24d27d321c9fc25222d07601
Victor <vpanya97@gmail.com>
2022-02-17 17:44:48 +0200

    add counter of Smileys by filter and regex

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
-
-function repeatStr (n, s) {
-    
-    console.log(s.repeat(n));
-  }
-
-  repeatStr(5, "I");
-
+let faces = [':D',':~)',';~D',':)',':O',':;']; //4
+function countSmileys(arr) {
+    return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+countSmileys(faces);
 /** 
+_________________________________________________________
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+    sol:
+let faces = [':D',':~)',';~D',':)',':O',':;']; //4
+function countSmileys(arr) {
+    return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+countSmileys(faces);
 _________________________________________________________
 Write a function called repeatStr which repeats the given string string exactly 
 n times.
